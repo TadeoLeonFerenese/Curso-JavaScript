ff65d3c0be90e1503517d086c12bfa56c0d08aca
Tadeo <teoleon2013@gmail.com>
2022-08-25 15:47:06 -0300

    cuarto commit

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,1 @@
+console.log(minOMBRE); // ESTO LO LLAMO DESDE EL DIRECTORIO
---
+++ 01-Fundamentos/assets/js/comentarios-variables.js
@@ -0,0 +1,42 @@
+// alert("Hola desde app.js");
+
+// console.log("Hola Mundo");
+
+// Lo que quiero comentar va aca adentro
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// let d = 10;
+// let x = a + b;
+
+//--------- o se puede hacer ne linea tambien -----------------//
+let a = 10,
+  b = 20,
+  c = "Hola",
+  d = "Spiderman",
+  x = a + b;
+
+const saludo = c + d;
+
+// console.log("%c Mis Variables", "color:blue; font-wiight: bold"); // sirve para darle styles a las varialbes de JS
+// console.log({ x });
+// console.log({ b });
+// console.log({ c });
+// console.log({ d });
+
+c = " Hola Mundo";
+
+// console.table({ a, b, c, d, x }); //ESTO TE DA LOS DATOS/VALORES DESEADOS EN UNA TABLA
+
+var minOMBRE = "Tadeo"; // ESTO ES UNA MALA PRACTICA !!!
+// var b = 10;
+// const c = 10;
+
+//La diferencia entre let, const y var
+
+// Const no te deja reasignar valores
+// var te deja reasignar el valor de sus valores siempre pero es la forma vieja de hacer valores
+// let te deja reasignar pero de forma local
+
+//EL MODO DEBUG SE PUEDE UTILIZAR PARA VERIFICAR EL FUNCIONAMIENTO PUNTO POR PUNTO EN EL CODIGO
