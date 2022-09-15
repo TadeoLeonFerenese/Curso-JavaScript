ec0704993fd5d961899d9acdad4786b5200bae1f
Tadeo <teoleon2013@gmail.com>
2022-09-15 15:43:00 -0300

    clase 49

---
+++ 02-BlackJack/assets/js/juego.js
@@ -0,0 +1,14 @@
+/**
+ * "2C = two  of clubs (treboles)"
+ * "2D = two  of diamonds (treboles)"
+ * "2H = two  of hearts (treboles)"
+ * "2S = two  of sapdes (treboles)"
+ */
+
+//reglas: hay que alcanzar los 21 puntos o acercarnos, si nos pasamos perdemos
+
+let deck = []; //aca podes crear un array con todas las cartas pero no es eficiente y es mala practica
+
+const crearDeck = () => {};
+
+crearDeck();
