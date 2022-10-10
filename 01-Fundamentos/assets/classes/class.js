dd7143d58888caeb897a9403a5906306cd6f6ed6
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2022-10-10 20:04:33 -0300

    prox class + subclass

--- 01-Fundamentos/assets/classes/class.js
+++ 01-Fundamentos/assets/classes/class.js
@@ -1,6 +1,15 @@
 // Clases
 
 class Persona {
+  static _conteo = 0;
+  static get conteo() {
+    return Persona._conteo + " instancias";
+  }
+  //creo un metodo sin hacer referencia al instanciamiento de la clase
+  static mensaje() {
+    console.log(this.nombre); //Undefined, no se puede hacer referencia al constructor
+    console.log("Hola a todos soy un metodo estatico");
+  }
   // por defecto vienen "use strict", osea que no podes hacer referencia en el constructor a una var en el scope
   nombre = "";
   codigo = "";
@@ -15,6 +24,8 @@ class Persona {
     this.codigo = codigo;
     this.nombre = nombre;
     this.frase = frase;
+    //Hago referencia a la class misma y no a la instancia que seria con "this"
+    Persona._conteo++;
   }
   //<----Sets and Gets---->
   // estos sirven para que cada vz que se establezca un valor por ej en "comida" que es una propiedad pueda controlar como se establece
@@ -41,11 +52,20 @@ const spiderman = new Persona(
   "Spider",
   "Soy tu amigable vecino Spiderman"
 );
-const ironman = new Persona("Tont Stark", "Iron", "Soy rico y vos no pobre");
-console.log(spiderman);
-console.log(ironman);
+// const ironman = new Persona("Tont Stark", "Iron", "Soy rico y vos no pobre");
+
+// Persona._conteo = 2;
+// console.log(spiderman);
+// console.log(ironman);
 
 spiderman.quienSoy();
-spiderman.miFrase();
+// spiderman.miFrase();
 spiderman.setComidaFavorita = "La torta de cereza de la tia May";
-console.log(spiderman.getComidaFavorita);
+// console.log(spiderman.getComidaFavorita);
+console.log("conteo estatico", Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+Persona.propiedadExterna = "Hola mundo";
+console.log(Persona.propiedadExterna);
+console.log(Persona);
