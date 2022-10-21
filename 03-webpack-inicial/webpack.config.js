38b1470751cad5126ec25bf4a74d7e1766d19e83
Tadeo <teoleon2013@gmail.com>
2022-10-21 13:39:16 -0300

    clase 89 ready paaappuuuu

--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -1,9 +1,29 @@
+const HtmlWebpack = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
-  modules: {
-    rules: [],
-  },
-  oprimization: [],
 
-  plugin: [],
+  output: { clean: true },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          sources: false, //se deja en false por que mueve automaticamente archivos y por eso se deja en False
+        },
+      },
+    ],
+  },
+
+  optimization: {},
+
+  plugins: [
+    new HtmlWebpack({
+      title: "Mi webpack App",
+      // filename: "holamundo.html",
+      template: "./src/index.html",
+    }),
+  ],
 };
