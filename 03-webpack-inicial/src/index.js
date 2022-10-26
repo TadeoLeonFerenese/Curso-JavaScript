8ffaea1093c5289bc6fd623565c2ebd90ae328d0
Tadeo <teoleon2013@gmail.com>
2022-10-26 15:52:06 -0300

    Averiguar el por que del Hola mundo de clase 90

--- 03-webpack-inicial/src/index.js
+++ 03-webpack-inicial/src/index.js
@@ -1,6 +1,6 @@
 //Importo la funcion "saludar del archivo  Componentes "
 import { saludar } from "./js/componentes.js"; //Esto sirve para importar como en react pero de esta forma es webpack manual
-
+import "./styles.css";
 const nombre = "Tadeo";
 
 saludar(nombre);
--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpack = require("html-webpack-plugin");
+const MiniCssExtract = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -16,7 +17,12 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        exclude: /styles.css$/, //se aplica este exclude por que testea en cascada y si cumple una regla no cumple la otra
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /styles.css$/,
+        use: [MiniCssExtract.loader, "css-loader"],
       },
     ],
   },
@@ -27,10 +33,11 @@ module.exports = {
     new HtmlWebpack({
       title: "Mi webpack App",
       template: "./src/index.html",
-      filename: "./index.html",
-      // title: "Mi webpack App",
-      // // filename: "holamundo.html",
-      // template: "./src/index.html",
+    }),
+
+    new MiniCssExtract({
+      filename: "[name].css",
+      ignoreOrder: false,
     }),
   ],
 };
