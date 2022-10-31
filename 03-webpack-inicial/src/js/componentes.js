7cee6fcb2080d778853eeb3b16f90abfff5478ea
Tadeo <teoleon2013@gmail.com>
2022-10-31 14:56:22 -0300

    seccion 8 ready

--- 03-webpack-inicial/src/js/componentes.js
+++ 03-webpack-inicial/src/js/componentes.js
@@ -1,7 +1,7 @@
 import "../css/componentes.css";
-import webpacklogo from "../assets/img/webpack-logo.png";
+// import webpacklogo from "../assets/img/webpack-logo.png";
 
-export const saludar = (nombre) => {
+export const saludar = (nombre = "sin nombre") => {
   console.log("Creando etiqueta h1");
 
   const h1 = document.createElement("h1");
@@ -9,8 +9,8 @@ export const saludar = (nombre) => {
 
   document.body.append(h1);
 
-  //IMG
-  const img = document.createElement("img");
-  img.src = webpacklogo;
-  document.body.append(img);
+  // //IMG
+  // const img = document.createElement("img");
+  // img.src = webpacklogo;
+  // document.body.append(img);
 };
