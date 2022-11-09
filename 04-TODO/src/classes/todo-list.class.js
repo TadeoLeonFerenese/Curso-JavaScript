aecb946b5fdce94644e9466dfe8618654f37fe39
Tadeo <teoleon2013@gmail.com>
2022-11-09 15:32:51 -0300

    Buscar error en BorrarTodosCompletados

--- 04-TODO/src/classes/todo-list.class.js
+++ 04-TODO/src/classes/todo-list.class.js
@@ -7,9 +7,21 @@ export class TodoList {
     this.todos.push(todo);
   }
 
-  eliminarTodo(id) {}
+  eliminarTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id != id);
+  }
 
-  marcarCompletado(id) {}
+  marcarCompletado(id) {
+    for (const todo of this.todos) {
+      if (todo.id == id) {
+        //La logica es que si la tarea esta sin comlletar por defecto es "False" pero sino es el contrario "!todo.completado" osea True
+        todo.completado = !todo.completado;
+        break;
+      }
+    }
+  }
 
-  eliminarCompletados() {}
+  eliminarCompletados() {
+    this.todos = this.todos.filter((todo) => !todo.completado);
+  }
 }
--- 04-TODO/src/js/componentes.js
+++ 04-TODO/src/js/componentes.js
@@ -5,6 +5,7 @@ import { todoList } from "../index";
 // Referencias en el html
 const divTodoList = document.querySelector(".todo-list");
 const txtInput = document.querySelector(".new-todo");
+const btnBorrar = document.querySelector(".clear-completed");
 
 export const crearTodoHtml = (todo) => {
   const htmlTodo = `
@@ -36,5 +37,33 @@ txtInput.addEventListener("keyup", (event) => {
 
     crearTodoHtml(nuevoTodo);
     txtInput.value = "";
+  }
+});
+
+divTodoList.addEventListener("click", (event) => {
+  const nombreElemento = event.target.localName; //Input, Label, Button
+  const todoElemento = event.target.parentElement.parentElement;
+  const todoId = todoElemento.getAttribute("data-id");
+
+  if (nombreElemento.includes("input")) {
+    //Click en el check
+    todoList.marcarCompletado(todoId);
+    todoElemento.classList.toggle("completed");
+  } else if (nombreElemento.includes("button")) {
+    //hay que borrar el todo
+
+    todoList.eliminarTodo(todoId);
+    divTodoList.removeChild(todoElemento);
+  }
+});
+
+btnBorrar.addEventListener("click", () => {
+  todoList.eliminarCompletados();
+
+  for (let i = divTodoList.children.lenght - 1; i >= 0; i--) {
+    const elemento = divTodoList.children[i];
+    if (elemento.classList.contains("completed")) {
+      divTodoList.removeChild(elemento);
+    }
   }
 });
