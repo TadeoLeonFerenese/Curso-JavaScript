cff69597b1182859c1f427e29bc1373b4a12c3d0
Tadeo <teoleon2013@gmail.com>
2023-02-01 15:47:42 -0300

    things

--- 05-advanced/src/conceptos/03-promises.js
+++ 05-advanced/src/conceptos/03-promises.js
@@ -11,6 +11,13 @@ export const promiseComponent = (element) => {
     element.innerHTML = hero.name;
   };
 
+  const renderTwoHero = (hero1, hero2) => {
+    element.innerHTML = `
+   <h3>${hero1.name}</h3>
+   <h3>${hero2.name}</h3>
+  `;
+  };
+
   const renderError = (error) => {
     element.innerHTML = `
     <h1>Error:</h1>
@@ -20,12 +27,40 @@ export const promiseComponent = (element) => {
   const id1 = "5d86371fd55e2e2a30fe1ccb2";
   const id2 = "5d86371fd55e2e2a30fe1ccb4";
 
+  //! Forma 3 (Solo se puede usar si las promesas no dependen la una de la otra)
+  Promise.all([findhero(id1), findhero(id2)])
+    .then(([hero1, hero2]) => {
+      renderTwoHero(hero1, hero2);
+    })
+    .catch(renderError);
+
+  //! Forma2
+  // let hero1;
+  // findhero(id1)
+  //   .then((hero) => {
+  //     hero1 = hero;
+  //     return findhero(id2);
+  //   })
+  //   .then((hero2) => {
+  //     renderTwoHero(hero1, hero2);
+  //   })
+  //   .catch(renderError);
+
+  //! THEN, CATCH, FINALLY
   //then sirve para cuando esta todo bn
   //catch cuando esta todo mal
   //finally es para terminar
-  findhero(id1)
-    .then(renderHero) //esta linea sirve para consumir la pomesa
-    .catch((error) => renderError(error)); //esta linea sirve para ejecutar en caso de error de busqueda
+  // findhero(id1)
+  //   .then((hero) => {
+  //     hero1 = hero2;
+
+  //! Forma 1
+  //     findhero(id2).then((hero2) => {
+  //       renderTwoHero(hero1, hero2);
+  //     })
+  //     .catch(renderError);
+  //   }) //esta linea sirve para consumir la pomesa
+  //   .catch((error) => renderError(error)); //esta linea sirve para ejecutar en caso de error de busqueda
 };
 
 /**
