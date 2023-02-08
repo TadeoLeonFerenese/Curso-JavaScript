6714490e1574c21ae9b5315dd5d83fdcbc0794e7
Tadeo <teoleon2013@gmail.com>
2023-02-08 15:52:06 -0300

    asd

--- 06-http-app/src/breakingbad/breakingbad-app.js
+++ 06-http-app/src/breakingbad/breakingbad-app.js
@@ -1,3 +1,13 @@
+//peticion HTTP desde una api
+
+/**
+ * @returns {Object} Quote information
+ */
+const fetchQuote = async () => {
+  const res = await fetch(" https://digi-api.com/api/v1/digimon "); //metodo para realizar una peticion http
+  console.log(res);
+};
+
 /**
  *
  * @param {HTMLDivElement} element
@@ -5,4 +15,6 @@
 export const BreakingBadApp = (element) => {
   document.querySelector("#app-title").innerHTML = "Breakingbad App";
   element.innerHTML = "Loading ...";
+
+  fetchQuote();
 };
