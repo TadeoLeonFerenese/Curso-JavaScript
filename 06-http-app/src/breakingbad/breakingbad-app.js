fbe77da1c315541d4260554bf6efe49365d2cf33
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2023-02-12 17:00:49 -0300

    A esta aplicacion estaria bueno cambiarle el style y agregarle un historial

--- 06-http-app/src/breakingbad/breakingbad-app.js
+++ 06-http-app/src/breakingbad/breakingbad-app.js
@@ -4,17 +4,39 @@
  * @returns {Object} Quote information
  */
 const fetchQuote = async () => {
-  const res = await fetch(" https://digi-api.com/api/v1/digimon "); //metodo para realizar una peticion http
-  console.log(res);
+  const res = await fetch(" https://api.breakingbadquotes.xyz/v1/quotes "); //metodo para realizar una peticion http
+  const data = await res.json(); //El Json se utiliza para obtener el bodi de la data (api)
+
+  console.log(data[0]);
+  return data[0];
 };
 
 /**
  *
  * @param {HTMLDivElement} element
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async (element) => {
   document.querySelector("#app-title").innerHTML = "Breakingbad App";
   element.innerHTML = "Loading ...";
+  // await fetchQuote();
 
-  fetchQuote();
+  const quoteLabel = document.createElement("blockquote");
+  const authoLabel = document.createElement("h3");
+  const nextQuoteButton = document.createElement("button");
+  nextQuoteButton.innerText = "Next Quote";
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.quote;
+    authoLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authoLabel, nextQuoteButton);
+  };
+
+  //Añadir Listener
+  nextQuoteButton.addEventListener("click", async () => {
+    element.innerHTML = "Loading ...";
+    const quote = await fetchQuote();
+    renderQuote(quote);
+  });
+
+  fetchQuote().then(renderQuote);
 };
